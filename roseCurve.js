06c926c67027b5e2045e805e3864384aaa143394
ALLISON G. MARTINEZ-AROCHO <martinga@us.ibm.com>
2017-03-12 22:10:50 -0400

    pulling down

--- roseCurve.js
+++ roseCurve.js
@@ -1,20 +1,24 @@
 var a = 0;
 var n = Math.floor((Math.random() * 8) + 1);
 var d = Math.floor((Math.random() * 8) + 1);
+var strokeColor = '#b9ffb1';
+var bgColor = '#022d41';
+var enhancer = 40;
+var dimension = 80;
 
 function setup() {
-  var canvas = createCanvas(80,80);
+  var canvas = createCanvas(dimension, dimension);
   canvas.parent('roseCurve');
 }
 
 function draw() {
 
   translate((width / 2), (height / 2));
-  stroke('#b9ffb1');
+  stroke(strokeColor);
   strokeWeight(2);
 
   if(a < (TWO_PI * d)) {
-    var r = 40 * cos((n / d) * a);
+    var r = enhancer * cos((n / d) * a);
     var x = r * cos(a);
     var y = r * sin(a);
     point(x, y);
@@ -26,5 +30,5 @@ function mouseClicked() {
     a = 0;
     n = Math.floor((Math.random() * 8) + 1);
     d = Math.floor((Math.random() * 8) + 1);
-    background('#022d41');
+    background(strokeColor);
   }
